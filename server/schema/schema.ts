040bb0a5f58641c7195920f04520b9f11569aa15
Samuel Gustafsson <samuelgustafsson890623@gmail.com>
2017-12-12 09:29:39 +0100

    import change and formatted with prettier.

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -1,11 +1,9 @@
-const _ = require('lodash');
-const graphql = require('graphql');
-const { GraphQLSchema } = graphql;
+import { GraphQLSchema } from "graphql";
 
-const RootQueryType = require('./root_query_type');
-const mutations = require('./mutations');
+const RootQueryType = require("./root_query_type");
+const mutations = require("./mutations");
 
 module.exports = new GraphQLSchema({
-  query: RootQueryType,
-  mutation: mutations
+	query: RootQueryType,
+	mutation: mutations,
 });
