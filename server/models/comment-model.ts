e2238a667a9cb5fc1d8890d6c5cd63a4fab6f722
SamuelGustafsson <samuelgustafsson890623@gmail.com>
2017-12-15 14:33:05 +0100

    Comment-schema.

--- server/models/comment-model.ts
+++ server/models/comment-model.ts
@@ -1,19 +1,16 @@
 import { Schema, model, Document } from "mongoose";
 
 export interface IComment extends Document {
-	readonly text: string;
-	readonly date: Date;
-	readonly author: string;
-	readonly userId: string;
-	readonly userImage?: string;
+  readonly text: string;
+  readonly date: Date;
+  readonly author: string;
 }
 
 export const commentSchema = new Schema({
-	text: { type: String, required: true },
-	date: { type: Date, default: Date.now },
-	author: { type: String, required: true },
-	userImage: String,
-	userId: Schema.Types.ObjectId,
+  text: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  author: { type: String, required: true },
+  userId: Schema.Types.ObjectId
 });
 
 export const CommentModel = model<IComment>("Comment", commentSchema);
