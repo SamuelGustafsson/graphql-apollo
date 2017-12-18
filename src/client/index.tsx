39125a597874a7bf1dadf920c1f1fe15741d2432
Samuel Gustafsson <samuelgustafsson890623@gmail.com>
2017-12-18 16:55:20 +0100

    apolloclient

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,9 +1,21 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-// import ApolloClient from "apollo-client";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const apolloClient = new ApolloClient({
+	link: new HttpLink({ uri: "http://localhost:8080/graphql" }),
+	cache: new InMemoryCache(),
+});
 
 const Root = () => {
-	return <div>Katt</div>;
+	return (
+		<ApolloProvider client={apolloClient}>
+			<div>Katt</div>;
+		</ApolloProvider>
+	);
 };
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
